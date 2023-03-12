052a5c2dc09739d7eb8a3e46ee7297183a485116
Eric <coffeenathaneric@yahoo.com>
2023-03-11 18:34:33 -0600

    Can now write text to screen. Also resized grid from 16x16 to 10x10.

--- main.py
+++ main.py
@@ -2,39 +2,42 @@ import pygame
 import sys
 from pygame.locals import *
 
+import font_manager
 import spritesheet
 
-FPS = 30 
+FPS = 30
 WINWIDTH = 640
 WINHEIGHT = 480
+GRIDSIZE = 10
 
 BLACK = (000, 000, 000)
 WHITE = (255, 255, 255)
 
 BLOCKS = [
 	(160, 160),
-	(176, 176),
-	(160, 176),
+	(170, 170),
+	(160, 170),
 ]
 
-def get_sprites_from_spritesheet(image_file):
+def get_sprite_from(image_file):
 	SPRITES = {
-		"player_left":  spritesheet.spritesheet(image_file).image_at(( 0,  0, 16, 16), colorkey = 0),
-		"player_up":    spritesheet.spritesheet(image_file).image_at((16,  0, 16, 16), colorkey = 0),
-		"player_right": spritesheet.spritesheet(image_file).image_at((32,  0, 16, 16), colorkey = 0),
-		"player_down":  spritesheet.spritesheet(image_file).image_at((48,  0, 16, 16), colorkey = 0),
-		"block":        spritesheet.spritesheet(image_file).image_at((64,  0, 16, 16), colorkey = 0),
+		"player_left":  spritesheet.spritesheet(image_file).image_at(( 0,  0, 10, 10), colorkey = 0),
+		"player_up":    spritesheet.spritesheet(image_file).image_at((10,  0, 10, 10), colorkey = 0),
+		"player_right": spritesheet.spritesheet(image_file).image_at((20,  0, 10, 10), colorkey = 0),
+		"player_down":  spritesheet.spritesheet(image_file).image_at((30,  0, 10, 10), colorkey = 0),
+		"block":        spritesheet.spritesheet(image_file).image_at((40,  0, 10, 10), colorkey = 0),
 	}
 	return SPRITES
 
 def main():
-	global FPSCLOCK, DISPLAYSURF
+	global FPSCLOCK, DISPLAYSURF, BASICFONT
 	pygame.init()
 	FPSCLOCK = pygame.time.Clock()
 	# pygame.display.set_icon(pygame.image.load(".png")) TODO
 	DISPLAYSURF = pygame.display.set_mode((WINWIDTH, WINHEIGHT))
 	pygame.display.set_caption("Title")
-	# BASICFONT = pygame.font.Font("freesansbold.ttf", 32)
+	BASICFONT = font_manager.get_font_from("font.png") # This is a dictionary whose keys are characters
+													   # and whose values are sprites of those characters.
 	while True:
 		runGame()
 
@@ -59,9 +62,10 @@ def runGame():
 	while True:
 		# current_time = FPSCLOCK.get_time()
 		DISPLAYSURF.fill(WHITE)
-		DISPLAYSURF.blit(get_sprites_from_spritesheet("spritesheet.png")[player_sprite], (playerx,playery))
+		DISPLAYSURF.blit(get_sprite_from("spritesheet.png")[player_sprite], (playerx,playery))
 		for block in BLOCKS:
-			DISPLAYSURF.blit(get_sprites_from_spritesheet("spritesheet.png")["block"], block)
+			DISPLAYSURF.blit(get_sprite_from("spritesheet.png")["block"], block)
+		font_manager.write_message(DISPLAYSURF, BASICFONT, "Test. TEST. 01234. \\□\\\\", 160, 320)
 
 		for event in pygame.event.get(): # event handling loop
 			if event.type == QUIT:
@@ -75,27 +79,27 @@ def runGame():
 		if keys[K_UP] and not moving:
 			player_sprite = "player_up"
 			move_delay_count += 1
-			if move_delay_count >= move_delay and place_free(playerx, playery - 16):
+			if move_delay_count >= move_delay and place_free(playerx, playery - GRIDSIZE):
 				moving = True
-				targety -= 16
+				targety -= GRIDSIZE
 		if keys[K_DOWN] and not moving:
 			player_sprite = "player_down"
 			move_delay_count += 1
-			if move_delay_count >= move_delay and place_free(playerx, playery + 16):
+			if move_delay_count >= move_delay and place_free(playerx, playery + GRIDSIZE):
 				moving = True
-				targety += 16
+				targety += GRIDSIZE
 		if keys[K_LEFT] and not moving:
 			player_sprite = "player_left"
 			move_delay_count += 1
-			if move_delay_count >= move_delay and place_free(playerx - 16, playery):
+			if move_delay_count >= move_delay and place_free(playerx - GRIDSIZE, playery):
 				moving = True
-				targetx -= 16
+				targetx -= GRIDSIZE
 		if keys[K_RIGHT] and not moving:
 			player_sprite = "player_right"
 			move_delay_count += 1
-			if move_delay_count >= move_delay and place_free(playerx + 16, playery):
+			if move_delay_count >= move_delay and place_free(playerx + GRIDSIZE, playery):
 				moving = True
-				targetx += 16
+				targetx += GRIDSIZE
 
 		if targetx > playerx: # Target to the right
 			playerx += 2 
