f5c9f97d222823127403a27468bc77bded7a63b3
Eric <coffeenathaneric@yahoo.com>
2023-03-25 18:54:45 -0500

    Refactoring via the functions dom() and fd()

--- entity_list.py
+++ entity_list.py
@@ -6,21 +6,32 @@ import entities as en
 #	}
 # But that's annoying and fiddly so let's auto-format it:
 
-def frmt(
-	*args,
-	flag = None,
-):
-	"""Takes the inputs and formats it as per the comment block above."""
+def dom(*args, flag = None,):
+	"""Takes the inputs and formats it as per the comment block above.
+	It's called 'dom' for 'dict_of_messages'."""
 	return [[arg for arg in args],flag]
+
+# flag_dict is formatted as follows:
+#	{
+#		flag: [[key1, dom1], [key2, dom2], ... ], 
+#	}
+# It's a list of tuples where you intend toreplace the keyn with domn when the flag is made true.
+# Again, let's auto-format that.
+
+def fd(*args):
+	"""Takes the inputs and formats it as per the comment block above.
+	It's called 'fd' for 'flag_dict'."""
+	return [[args[i], args[i+1]] for i in range(0, len(args), 2)]
+
 
 guy1 = en.Entity(
 	name = "Guy1", 
 	x = 0, 
 	y = 20,
 	dict_of_messages = {
-		0: frmt("Hello. This is message 0.", 1),
-		1: frmt("Welcome back. This is message 1.", 2),
-		2: frmt("Now this is message 2.", 0),
+		0: dom("Hello. This is message 0.", 1),
+		1: dom("Welcome back. This is message 1.", 2),
+		2: dom("Now this is message 2.", 0),
 	},
 	dict_key = 0,
 	flag_dict = {},
@@ -32,9 +43,9 @@ guy2 = en.Entity(
 	x = -20, 
 	y = -10,
 	dict_of_messages = {
-		0: frmt("This is message 0.", 2),
-		1: frmt("This is message 1.", 0),
-		2: frmt("We skipped to message 2. You'll go to 1 next.", 1),
+		0: dom("This is message 0.", 2),
+		1: dom("This is message 1.", 0),
+		2: dom("We skipped to message 2. You'll go to 1 next.", 1),
 	},
 	dict_key = 0,
 	flag_dict = {},
@@ -46,10 +57,18 @@ guy3 = en.Entity(
 	x = 0, 
 	y = -30,
 	dict_of_messages = {
-		0: frmt("Yes or No?", "1Yes. 2Yes. 3Yes. 4Yes. 5Yes. 6Yes. 7Yes. 8Yes. 9Yes. 10Yes. 11Yes. 12Yes. 13Yes. 14Yes. 15Yes. 16Yes. 17Yes. 18Yes. 19Yes. 20Yes.\
-			1Yes. 2Yes. 3Yes. 4Yes. 5Yes. 6Yes. 7Yes. 8Yes. 9Yes. 10Yes. 11Yes. 12Yes. 13Yes. 14Yes. 15Yes. 16Yes. 17Yes. 18Yes. 19Yes. 20Yes.", 1, "No.", 2),
-		1: frmt("You said yes.", 0),
-		2: frmt("You said no.", 0),
+		0: dom("Yes or No?", 
+			"1Yes. 2Yes. 3Yes. 4Yes. 5Yes. \
+			6Yes. 7Yes. 8Yes. 9Yes. 10Yes. \
+			11Yes. 12Yes. 13Yes. 14Yes. 15Yes. \
+			16Yes. 17Yes. 18Yes. 19Yes. 20Yes.\
+			1Yes. 2Yes. 3Yes. 4Yes. 5Yes. \
+			6Yes. 7Yes. 8Yes. 9Yes. 10Yes. \
+			11Yes. 12Yes. 13Yes. 14Yes. 15Yes. \
+			16Yes. 17Yes. 18Yes. 19Yes. 20Yes.", 1, 
+			"No.", 2),
+		1: dom("You said yes.", 0),
+		2: dom("You said no.", 0),
 	},
 	dict_key = 0,
 	flag_dict = {},
@@ -61,8 +80,12 @@ guy4 = en.Entity(
 	x = 30, 
 	y = -30,
 	dict_of_messages = {
-		0: frmt("Here's a really long message. We want to continue to confirm that wrapping the message across multiple boxes works. This message is quite long. Let's wrap it up. Here is the final sentence of the message.", 1),
-		1: frmt("This one's short.", 0),
+		0: dom("Here's a really long message. \
+			We want to continue to confirm that wrapping the \
+			message across multiple boxes works. This message \
+			is quite long. Let's wrap it up. Here is the final \
+			sentence of the message.", 1),
+		1: dom("This one's short.", 0),
 	},
 	dict_key = 0,
 	flag_dict = {},
@@ -74,11 +97,11 @@ guy5 = en.Entity(
 	x = 50, 
 	y = 50,
 	dict_of_messages = {
-		0: frmt("You have not talked to my neighbor.", 0),
+		0: dom("You have not talked to my neighbor.", 0),
 	},
 	dict_key = 0,
 	flag_dict = {
-		"test_flag": [[0, frmt("Now you have talked to my neighbor!", 0)]],
+		"test_flag": fd(0, dom("Now you have talked to my neighbor!",0)),
 	},
 	in_conversation = False,
 	schedule = None,
@@ -88,7 +111,7 @@ guy6 = en.Entity(
 	x = 60, 
 	y = 50,
 	dict_of_messages = {
-		0: frmt("This triggered the flag.", 0, flag="test_flag"),
+		0: dom("This triggered the flag.", 0, flag="test_flag"),
 	},
 	dict_key = 0,
 	flag_dict = {},
@@ -100,13 +123,16 @@ guy7 = en.Entity(
 	x = 70, 
 	y = 50,
 	dict_of_messages = {
-		0: frmt("Did you talk to my neighbor? Yes or no?", "Yes.", 1, "No.", 2),
-		1: frmt("Liar!", 0),
-		2: frmt("Well you should.", 0),
+		0: dom("Did you talk to my neighbor? Yes or no?", "Yes.", 1, "No.", 2),
+		1: dom("Liar!", 0),
+		2: dom("Well you should.", 0),
 	},
 	dict_key = 0,
 	flag_dict = {
-		"test_flag": [[1, frmt("Good!", 0)], [2, frmt("My neighbor said you did though. Don't lie!", 0)]],
+		"test_flag": fd(
+						1, dom("Good!",0), 
+						2, dom("My neighbor said you did though. Don't lie!",0),
+						),
 	},
 	in_conversation = False,
 	schedule = None,
